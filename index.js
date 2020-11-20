cfec1d70b50f89f058be9f372b6e831730fd53d5
jovial-application-1943 <fatima.katrib@lau.edu>
2020-11-20 20:26:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const append=["Milo", "Otis", "Garfield"]
 const prepend=["Milo", "Otis", "Garfield"]
 const removeLast=["Milo", "Otis", "Garfield"]
 const removeFirst=["Milo", "Otis", "Garfield"]
-append.append("Odie")
-prepend.prepend("Odie")
+append.push("Odie")
+prepend.unshift("Odie")
+removeLast.pop()
+removeFirst.shift()
